e9c64b3d7516be1fe59200c6d124d1b2009b4208
nabeel <nabeels91@gmail.com>
2017-08-18 14:51:43 +0100

    ch5 done--baddddly

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
-
-#rout_path// root_url#
-
-  get  '/home', to: 'static_pages#home'
-
-  get get  '/help', to: 'static_pages#help'
-
-  get get  '/about', to: 'static_pages#about'
-
-  get get  '/contact', to: 'static_pages#contactus'
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
